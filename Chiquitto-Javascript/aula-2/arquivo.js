c8e4ee50cd6efbb2594e8101f7182341534d869b
joseadilson <joseadilsson@gmail.com>
2017-04-08 13:27:38 -0300

    aula-2-javascript

--- Chiquitto-Javascript/aula-2/arquivo.js
+++ Chiquitto-Javascript/aula-2/arquivo.js
@@ -49,3 +49,86 @@ for (let i = 0; i < 10; i++) {
 
 console.log(i)
 */
+
+
+//PROMISE
+/*
+let p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(2017);
+    }, 3000);
+});
+
+let p2 = p.then((result) => {
+    console.log(result);
+
+    return new Promise((resolve,reject) => {
+        setTimeout(() =>{
+            resolve(2018);
+        }, 3000);
+    });
+});
+
+p2.then((result) => {
+    console.log(result);
+});
+*/
+
+//Nome, email, cpf, idade, genero
+let p = new Promise((resolve, reject) => {
+    mostrarLoading();
+
+    reject(1);
+})
+.then(validarNome)
+.then(validarEmail)
+.then(validarCPF)
+.then(validarIdade)
+.then(validarGenero)
+.catch(tratarReject);
+
+function mostrarLoading() {
+    console.log('Mostrar Loading');
+}
+
+function validarNome(result){
+    console.log('Validar nome');
+
+    return result;
+}
+
+function validarEmail(result){
+    console.log('Validar email');
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(result);
+        }, 3000);
+    });
+}
+
+function validarCPF(result){
+    console.log('Validar CPF');
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(result);
+        }, 3000);
+    });
+}
+
+function validarIdade(result){
+    console.log('Validar idade');
+
+    return result;
+}
+
+function validarGenero(result){
+    console.log('Validar genero');
+
+    return result;
+}
+
+function tratarReject(reason){
+    console.log(reason);
+}
