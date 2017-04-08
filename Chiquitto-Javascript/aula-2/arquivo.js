32f4559edb0cca07276a0909c9e62f61d49fb6d7
joseadilson <joseadilsson@gmail.com>
2017-04-08 10:57:51 -0300

    aula-2-javascript

---
+++ Chiquitto-Javascript/aula-2/arquivo.js
@@ -0,0 +1,51 @@
+
+//IMC
+/*let imc = (peso, altura) => peso / (altura * altura);
+
+let imc = (peso, altura) => {
+   let r = peso / (altura * altura);
+   return r;
+}
+
+let resultudo  = imc(80, 1.75);
+console.log(resultudo);
+*/
+//
+
+/*
+var a, b, c;
+var vetor = [10,20,30,40];
+
+[a, b, ...c] = vetor;
+
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+
+/*
+function multiplicacao(a, b=2){
+    console.log(a);
+    console.log(b);
+}
+
+multiplicacao(10);
+*/
+
+/*
+function alfa(a, b, ...entrada){
+    console.log(a);
+    console.log(b);
+    console.log(entrada);
+}
+
+alfa('A','B','C','D');
+*/
+
+/*
+for (let i = 0; i < 10; i++) {
+    
+}
+
+console.log(i)
+*/
