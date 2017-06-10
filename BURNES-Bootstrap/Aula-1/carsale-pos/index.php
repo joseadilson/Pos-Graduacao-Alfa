bb76b1876a9a407de0cb9529a423d3c0d0761801
josé adilson <joseadilsson@gmail.com>
2017-06-10 10:58:54 -0300

    aula-1-bootstrap

---
+++ BURNES-Bootstrap/Aula-1/carsale-pos/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>CarSale</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Venda de carros">
+        <meta name="keywords" content="fiat, 147">
+
+        <!-- facebook - open grapgicss (og) -->
+        <meta propertty="og:title" content="Car Sale">
+        <meta propertty="og-locale" content="pt_BR">
+        <meta propertty="og-locale" content="pt_BR">
+        <meta propertty="og-description" content="Venda de carros">
+        <meta propertty="og-image" content="http://pos.professorburnes.com.br/carsale2/img/carsale.jpgs">
+        <meta propertty="og-image:width" content="800">
+        <meta propertty="og-image:height" content="315">
+
+        <!-- twitter cards -->
+        <meta name="twitter:url" content="@joseadilson">
+        <meta name="twitter:image" content="http://pos.professorburnes.com.br/carsale2/img/carsale.jpgs">
+        <meta name="twitter:title" content="Car Sale">
+
+        <!-- javascript -->
+        <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap-inputmask.min.js"></script>
+        <script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
+
+        <!-- css -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="icon" type="image/png" href="imgs/icone.png">
+
+
+
+    </head>
+    <body>
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                    <a href="index.php"></a>
+                    <img src="imgs/logo.png" alt="">                
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-6 text-right">
+                    000000000000000               
+                </div>
+            </div>
+        </div>
+    
+    </header>
+    
+    </body>
+</html>
