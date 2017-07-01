fd115415ec9238d2dc30c6b1edb7ff45e22be8a7
josé adilson <joseadilsson@gmail.com>
2017-07-01 10:52:24 -0300

    aula-2-col

--- BURNES-Bootstrap/Aula-2/carsale-pos/pages/home.php
+++ BURNES-Bootstrap/Aula-2/carsale-pos/pages/home.php
@@ -1,1 +1,87 @@
-<h1>Bem vindo a carsale</h1>
+<div class="container">   
+    
+   <h1>Bem vindo a carsale</h1>
+
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <img src="imgs/maverick.jpg" alt="Maverick" title="Maverick">
+                <h2>Maverick v8 1971</h2>
+                <p>Carro todo restaurado, mecânica toda feita, motor novo. Carburador Quadrijet 600 Edelbrock.</p>
+                <p class="text-center">
+                    <a href="maverick" class="btn btn-danger btn-lg">
+                        <i class="glyphicon glyphicon-search"></i> Detalhes
+                    </a>
+                </p>            
+            </div>        
+        </div>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <img src="imgs/corvete.jpg" alt="Corvete" title="Corvete">
+                <h2>Corvete 12V 1979</h2>
+                <p>Carro todo restaurado, mecânica toda feita, motor novo. Carburador Quadrijet 600 Edelbrock.</p>
+                <p class="text-center">
+                    <a href="corvete" class="btn btn-danger btn-lg">
+                        <i class="glyphicon glyphicon-search"></i> Detalhes
+                    </a>
+                </p>            
+            </div>        
+        </div>
+        
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <img src="imgs/landau.jpg" alt="Landaw" title="Landau">
+                <h2>Ford Landau 1971</h2>
+                <p>Carro todo restaurado, mecânica toda feita, motor novo. Carburador Quadrijet 600 Edelbrock.</p>
+                <p class="text-center">
+                    <a href="landau" class="btn btn-danger btn-lg">
+                        <i class="glyphicon glyphicon-search"></i> Detalhes
+                    </a>
+                </p>            
+            </div>        
+        </div>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <img src="imgs/opala.jpg" alt="Opala" title="Opala">
+                <h2>Ford Opala 1984</h2>
+                <p>Carro todo restaurado, mecânica toda feita, motor novo. Carburador Quadrijet 600 Edelbrock.</p>
+                <p class="text-center">
+                    <a href="opala" class="btn btn-danger btn-lg">
+                        <i class="glyphicon glyphicon-search"></i> Detalhes
+                    </a>
+                </p>            
+            </div>        
+        </div>
+        
+    </div>
+    
+</div>    
+
+<div id="sobre">
+    <div class="container">
+        <h3>Sobre a Carsale</h3>
+        <p>Deserunt quis eiusmod dolor magna sunt in ad.
+            Magna nulla tempor adipisicing nostrud eu elit ullamco consectetur nisi consectetur.
+            Nostrud elit incididunt cupidatat elit laboris duis deserunt consectetur cupidatat ex nostrud.
+            Tempor pariatur fugiat occaecat aliquip qui proident.</p>        
+    </div>
+</div>
+
+<div class="container">
+    <h3>Destaques</h3>  
+
+    <div class="row">
+        <div class="col-md-3 col-sm-6 text-center">
+            <img src="imgs/bmw.jpg" alt="BMW" title="BMW">
+            <h3>BMW 2001</h3>
+            <p class="text-center">
+                <a href="bmw" class="btn btn-danger">
+                    <i class="glyphicon glyphicon-search"></i> Detalhes
+                </a>
+            </p>
+        </div>        
+    </div>  
+    
+</div>
